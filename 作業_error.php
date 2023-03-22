f74cf3d942f0db9934df1bff60ff4fb3e0b1c39f
wanwan0u0 <125526823+wanwan0u0@users.noreply.github.com>
2023-03-22 14:48:53 +0800

    作業_error

---
+++ 作業_error.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+非法進入網頁!!!</br>
+網頁將在三秒後跳轉登入頁面</br>
+<a href="作業_使用者介面.php">點選這裡</a>
+<?php
+header("Refresh:3;url=http://127.0.0.1/%E4%BD%9C%E6%A5%AD_%E4%BD%BF%E7%94%A8%E8%80%85%E4%BB%8B%E9%9D%A2.php")
+?>
+</body>
+</html>
