c611dcf958187f0b51e5b42b52252cf94cb31162
wanwan0u0 <125526823+wanwan0u0@users.noreply.github.com>
2023-03-22 14:46:14 +0800

    作業_使用者介面

---
+++ 作業_使用者介面.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>作業_使用者介面</title>
+<meta charset="utf-8">
+</head>
+<body>
+<form action="作業_check.php" method="post">
+Your ID:<input type="text" name="id"></br>
+password:<input type="password" name="pwd"></br>
+<input type="submit"> <input type="reset">
+</form>
+</body>
+</html>
